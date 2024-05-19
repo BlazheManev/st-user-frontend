185eb0078326ea2e9a67a1e7c978921af9bdb4c2
Blazhe_Manev <manev.blazhe@student.um.si>
2024-05-19 11:49:39 +0200

    login component

--- src/components/login.component.tsx
+++ src/components/login.component.tsx
@@ -57,6 +57,7 @@ export default class Login extends Component<Props, State> {
         this.setState({
           redirect: "/profile"
         });
+        window.location.reload();
       },
       error => {
         const resMessage =
