993010e835901f35f594ee21f560ed7c9483065a
Blazhe_Manev <manev.blazhe@student.um.si>
2024-05-26 14:26:18 +0200

    Making PWA

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -14,6 +15,8 @@ root.render(
     <App />
   </BrowserRouter>
 );
+
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
